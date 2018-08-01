a900ac98502ae71df7f2b3cb9352b083ed245502
ayahammoud <40678812+ayahammoud@users.noreply.github.com>
2018-08-01 19:00:54 -0400

    Add files via upload

---
+++ Week 5/file.js
@@ -0,0 +1,70 @@
+// Q1 ->
+var english= "hello world // English";
+console.log(english)
+var arabic= "مرحباً أيها العالم! // Arabic";
+console.log(arabic)
+
+// Q2 ->
+console.log("I'm awesome")
+
+// Q3 ->
+var x;
+console.log('I think x value is...');
+console.log(x)
+x=6;
+console.log('I think x value is...');
+console.log(x)
+
+// Q4 ->
+var y=4;
+console.log('I think the value of the string is.. ');
+console.log(y)
+var y=2;
+console.log('I think the value of the string is.. ');
+console.log(y)
+
+// Q5 ->
+var z=7.25;
+console.log(z);
+var a=8;
+console.log(a)
+var thehighestvalue=8;
+console.log(thehighestvalue)
+
+// Q6 ->
+var names=[]
+console.log('the value of the array is...');
+console.log(names)
+var animals=["cat","dog","bird"]
+console.log(animals)
+animals[3]="kitten"
+console.log(animals)
+
+// Q7 ->
+let myString = "this is a test";
+console.log(myString)
+myString.length;
+console.log(myString.length)
+
+// Q8 ->
+var x=7;
+console.log(x)
+var y=3;
+console.log(y)
+x%y;
+console.log(x%y)
+
+// Q9 ->
+//a_
+// yes we can store multiple types in an array(numbers and things) for example:
+var person = ["aya", "hammoud", 19];
+// this array contain strings (first name:aya and last name:hammoud) and a number(age:19).
+//b_
+// yes we can compare infiniteis and 6/0 === 10/0 is true.
+//c_
+var x=6;
+var y=10;
+var z=0;
+console.log(x/z)
+console.log(y/z)
+console.log(x/z == y/z)
