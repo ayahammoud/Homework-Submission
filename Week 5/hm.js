092b384b50052f7155685babf45d949ac85471f8
ayahammoud <40678812+ayahammoud@users.noreply.github.com>
2018-08-08 15:31:27 -0400

    Add files via upload

---
+++ Week 5/hm.js
@@ -0,0 +1,16 @@
+// Q1 ->
+
+function vehicle(color, age,code){
+  var vehiclecolor = color;
+  var vehicleage = age;
+
+  console.log("the car is " + color + " and it is " + age + " years ")
+}
+
+vehicle("blue", 5,)
+
+
+//Q3-->
+
+var languagas = {javascript:"saad", html :"rajie" , css:"rajie" };
+console.log(languagas);
